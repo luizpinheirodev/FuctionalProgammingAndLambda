1317b0e3f179a6c7a34cb2f9ada9f609a5aa7e1d
Luiz Geraldo Pinheiro <luiz_geraldo@sicredi.com.br>
2019-07-14 11:39:16 -0300

    FirstCommit

---
+++ src/comparator/application/Program.java
@@ -0,0 +1,50 @@
+package comparator.application;
+
+import comparator.entity.Product;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        //form 1
+        //list.sort(new MyComparator());
+
+        //form2 (classe anonima)
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };
+        list.sort(comp);
+
+        //form3 Lambda(função anonima) - arrow function
+        Comparator<Product> comp2 = (p1, p2) -> {
+            return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+        };
+        list.sort(comp2);
+
+        //form4
+        Comparator<Product> comp3 = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+        list.sort(comp3);
+
+        //form5
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+
+    }
+
+}
